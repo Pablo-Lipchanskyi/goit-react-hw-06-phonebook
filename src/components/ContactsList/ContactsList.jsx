067dc6bb6088763f09b15cdd5165430eafc67e4f
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-02-04 12:48:26 +0200

    summ

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,32 +1,40 @@
-import PropTypes from 'prop-types';
-import { ButtonDelete } from 'components/Button/ButtonDelete';
-import css from 'components/ContactsList/contactList.module.css'
-export const ContactsList = ({ contacts, onDeleteButton }) => {
-  return (
-    <div className={css.contacts}>
-      {contacts.map(({ id, name, number }) => {
-        return (
-          <div key={id} className={css.marker}>
-            <div className={css.contactItem}>
-              {`${name}: ${number}`}
-              <ButtonDelete actionHandler={() => onDeleteButton(id)}>
-                X
-              </ButtonDelete>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+import { useSelector, useDispatch } from 'react-redux';
+import { remove, getContacts } from 'redux/contacts/contactsSlice';
+import { getFilter } from 'redux/filter/filterSlice';
+import { ContactsListStyled } from './ContactsList.styled';
+import { ContactsListItem } from 'components/ContactsListItem/ContactsListItem';
 
-ContactsList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.node.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.node.isRequired,
-    }).isRequired
-  ),
-  onDeleteButton: PropTypes.func.isRequired,
+export const ContactsList = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const dispatch = useDispatch();
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const visibleContacts = getVisibleContacts();
+
+  if (visibleContacts.length > 0) {
+    return (
+      <ContactsListStyled>
+        {visibleContacts.map(({ id, name, number }) => {
+          return (
+            <ContactsListItem
+              key={id}
+              id={id}
+              name={name}
+              number={number}
+              onDeleteButton={id => dispatch(remove(id))}
+            ></ContactsListItem>
+          );
+        })}
+      </ContactsListStyled>
+    );
+  }
 };
